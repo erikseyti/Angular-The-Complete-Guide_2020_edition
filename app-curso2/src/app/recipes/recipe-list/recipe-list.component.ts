5eabd0c295bce476b642f4657f8bf27b82d658fe
Erik Seyti <erikseyti@gmail.com>
2020-12-14 20:10:37 -0300

    Modificado componente para mostrar os detalhes da receita, criado estrutura basica do formulario para adicionar ingredientes e componente mostrando os ingredientes presentes no carrinho de compras

--- app-curso2/src/app/recipes/recipe-list/recipe-list.component.ts
+++ app-curso2/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is just a test',
-    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test Recipe 2', 'This is just a test 2',
+    'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg')
   ];
 
   constructor() { }
