423cee0852dc96f65afb2e42259b6adca943982d
Erik Seyti <erikseyti@gmail.com>
2021-03-29 20:14:51 -0300

    Example of Encapsulation of css
    Signed-off-by: Erik Seyti <erikseyti@gmail.com>

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // ViewEncapsulation.None means that any value on this component
+  // will be share to the rest of the components
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
