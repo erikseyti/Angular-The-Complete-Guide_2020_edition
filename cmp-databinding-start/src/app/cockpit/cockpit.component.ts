467996b618574212dfac2ca1bcde0f66413b5264
Erik Seyti <erikseyti@gmail.com>
2021-04-12 19:51:42 -0300

    Criado exemplo @ViewChild e projetar conteudo de um Component por meio do ng-content

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,6 +1,6 @@
 // import { ServersComponent } from './../../../../app-curso/src/app/servers/servers.component';
 // import { ServerComponent } from './../../../../app-curso/src/app/server/server.component';
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,7 +11,8 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContent', {static: true}) serverContentInput : ElementRef;
 
   constructor() { }
 
@@ -19,17 +20,19 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(nameInput: HTMLInputElement) {
-    console.log(nameInput.value);
+    console.log(this.serverContentInput);
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
+    // You can set a value to a localRef on ViewChild
+    // this.serverContentInput.nativeElement.value = "Something";
     this.bluePrintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
       }
     );
   }
