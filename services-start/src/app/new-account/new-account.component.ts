fd462ba20b7b8ab1efacea824376754a61ecd5fb
Erik Seyti <erikseyti@gmail.com>
2021-04-30 17:11:41 -0300

    create a cross-component communication via a service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -12,10 +12,15 @@ export class NewAccountComponent {
 
 
   constructor(private loggingService: LoggingService,
-    private accountService: AccountService) {}
+    private accountService: AccountService) {
+      this.accountService.statusUpdated.subscribe(
+        (status: string) => alert("New Status"+ status)
+      );
+    }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
     // this.loggingService.logStatusChange(accountStatus);
+
   }
 }
