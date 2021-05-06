555935e8411ce53723ba716d1b022dbf83a82086
Erik Seyti <erikseyti@gmail.com>
2021-05-06 20:42:43 -0300

    Add Existing Functions to Services and create add ingredients to Recipe type

--- app-curso2/src/app/recipes/recipe.service.ts
+++ app-curso2/src/app/recipes/recipe.service.ts
@@ -1,18 +1,41 @@
-import { Injectable } from "@angular/core";
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is just a test',
-    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test Recipe 2', 'This is just a test 2',
-    'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg')
+    new Recipe('Pork Stake with veggies',
+    'Is good for you!',
+    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    [
+      new Ingredient('Pork Meat', 1),
+      new Ingredient('Carrot', 1),
+      new Ingredient('Tomato', 1)
+    ]),
+    new Recipe('Fist of the North Star Tempura',
+    'Good to end the weekend!',
+    'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg',
+    [
+      new Ingredient('Flour', 1),
+      new Ingredient('Carrot', 1),
+      new Ingredient('Shrimp', 2)
+
+    ])
   ];
+
+  constructor(private slService: ShoppingListService)
+  {}
 
   getRecipes(){
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
   }
 
 
--- app-curso2/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ app-curso2/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingListService } from './../shopping-list.service';
 import { Ingredient } from './../../shared/ingredient.model';
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
@@ -9,10 +10,9 @@ import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '
 export class ShoppingEditComponent implements OnInit {
   @ViewChild("nameInput", {static: false}) nameInputRef: ElementRef;
   @ViewChild("amountInput", {static: false}) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +21,6 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient);
   }
 }
