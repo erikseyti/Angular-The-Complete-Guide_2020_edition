a869dd48284a0ae2036b9adbeccbe5641ab59867
Erik Seyti <erikseyti@gmail.com>
2021-05-21 19:36:12 -0300

    refac appCurso2 to use a better approuch for our services with Observables(Subjects)

--- app-curso2/src/app/shopping-list/shopping-list.service.ts
+++ app-curso2/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import {Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import {Subject} from 'rxjs';
 
 @Injectable()
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -16,7 +17,7 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
       this.ingredients.push(ingredient);
-      this.ingredientsChanged.emit(this.ingredients.slice());
+      this.ingredientsChanged.next(this.ingredients.slice());
 
     }
 
@@ -26,7 +27,7 @@ export class ShoppingListService {
       //   this.addIngredient(ingredient);
       // }
       this.ingredients.push(...ingredients);
-      this.ingredientsChanged.emit(this.ingredients.slice());
+      this.ingredientsChanged.next(this.ingredients.slice());
     }
 
 }
