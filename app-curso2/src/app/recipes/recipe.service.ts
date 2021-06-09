4372ddeb66adafc72fa90f546871961128b287fc
Erik Seyti <erikseyti@gmail.com>
2021-06-09 20:36:47 -0300

    On AppCurso2: Submitting the form from the recipe edit form

--- app-curso2/src/app/recipes/recipe.service.ts
+++ app-curso2/src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Subject } from 'rxjs';
 import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import {Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
@@ -5,6 +6,7 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe('Pork Stake with Veggies',
@@ -39,6 +41,16 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 
