32f137d0f88234d89d46894a4e523700ea3becc8
Erik Seyti <erikseyti@gmail.com>
2021-06-11 15:28:38 -0300

    On AppCurso2: create the functions to delete and cancel the Recipe

--- app-curso2/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ app-curso2/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,8 +2,7 @@ import { Ingredient } from './../../shared/ingredient.model';
 import { RecipeService } from './../recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Recipe } from '../recipe.model';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -16,7 +15,8 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-    private recipeService: RecipeService) { }
+    private recipeService: RecipeService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -40,6 +40,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onAddIngredient() {
@@ -51,6 +52,10 @@ export class RecipeEditComponent implements OnInit {
         ]  )
       })
     )
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route})
   }
 
   private initForm(){
