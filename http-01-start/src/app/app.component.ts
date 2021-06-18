dd32029e319a7b3c4b610e76b8492dbd51f27736
Erik Seyti <erikseyti@gmail.com>
2021-06-18 16:10:11 -0300

    Add Type Hinting on the HTTPClient requests for get and post

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +17,11 @@ export class AppComponent implements OnInit {
     this.fetchPost()
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     console.log(postData);
     // URL provided by the firebase Database by Google
-    this.http.post('https://ng-complete-course-3787b-default-rtdb.firebaseio.com/posts.json',
+    this.http.post<{name: string}>('https://ng-complete-course-3787b-default-rtdb.firebaseio.com/posts.json',
     postData).subscribe(responseData => {
       console.log(responseData);
     });
@@ -36,14 +37,13 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPost() {
-    this.http.get('https://ng-complete-course-3787b-default-rtdb.firebaseio.com/posts.json')
-    .pipe(map( responseData => {
-      const postsArray = [];
+    this.http.get<{[key: string]: Post}>('https://ng-complete-course-3787b-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map( (responseData) => {
+      const postsArray: Post[] = [];
       for (const key in responseData){
         if (responseData.hasOwnProperty(key)) {
           postsArray.push({...responseData[key], id: key})
-        }
-      }
+        }}
       return postsArray;
     }))
     .subscribe((posts)=> {
