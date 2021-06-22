1f09a5038279af4651f22674dbf7f391c32bf27c
Erik Seyti <erikseyti@gmail.com>
2021-06-22 09:21:58 -0300

    Example of manipulating a http request with an interceptor

--- http-01-start/src/app/auth-interceptor.serivce.ts
+++ http-01-start/src/app/auth-interceptor.serivce.ts
@@ -3,6 +3,11 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log('Request is on its way');
-    return next.handle(req);
+    console.log(req.url);
+    const modifiedReuqest = req.clone({
+      // Example of URL modification on the interceptor
+      // url: 'some-new-url',
+      headers: req.headers.append('Auth', 'xyz')});
+    return next.handle(modifiedReuqest);
   }
 }
