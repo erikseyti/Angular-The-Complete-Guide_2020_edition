82de0980c806c5e69aafada50718a82849393345
Erik Seyti <erikseyti@gmail.com>
2021-07-01 20:16:57 -0300

    On AppCurso2 create error handler for signUp and the login function of user in firebase

--- app-curso2/src/app/auth/auth.component.ts
+++ app-curso2/src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
+import { AuthResponseData, AuthService } from './auth.service';
 import { NgForm } from '@angular/forms';
 import { Component } from "@angular/core";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -7,12 +9,43 @@ import { Component } from "@angular/core";
 })
 export class AuthComponent{
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService){}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
   onSubmit(form: NgForm){
-    console.log(form.value);
+    // extra security to not send a form onSubmit if form is not corresponding correctly on browser
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+
+    let authObservable: Observable<AuthResponseData>;
+
+    if (this.isLoginMode) {
+     authObservable = this.authService.login(email, password);
+    }
+    else {
+     authObservable = this.authService.signUp(email, password)
+    }
+
+    authObservable.subscribe(
+      responseData => {
+      console.log(responseData);
+      this.isLoading = false;
+    }, errorMessage => {
+      console.log(errorMessage);
+      this.error = errorMessage;
+      this.isLoading = false;
+    });
+
     form.reset();
   }
 }
--- app-curso2/src/app/auth/auth.service.ts
+++ app-curso2/src/app/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData{
+export interface AuthResponseData{
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  kind: string;
+  registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -16,11 +20,32 @@ export class AuthService {
 
   signUp(email: string, password: string) {
     // Get the key on Firebase configuration on yout project
-   return this.http.post<AuthResponseData>('YouApiKey',
+   return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]',
       {email: email,
       password: password,
       returnSecureToken: true
       }
-    );
+    ).pipe(catchError(errorResponse => {
+      let errorMessage = 'an Unknow error ocurred!'
+      if (!errorResponse.error || !errorResponse.error.error){
+
+        return throwError(errorMessage);
+      }
+      switch (errorResponse.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'this email already exists.'
+      }
+
+      return throwError(errorMessage);
+    }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    });
   }
 }
