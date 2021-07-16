743a6e1f926929a5f4879f05a63d0c0a47a54a1b
Erik Seyti <erikseyti@gmail.com>
2021-07-16 17:54:41 -0300

    Implement Lazy Loading for the Recipes Module

--- app-curso2/src/app/app-routing.module.ts
+++ app-curso2/src/app/app-routing.module.ts
@@ -3,7 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'}
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes',
+  // old implementation of lazy loading.
+  // loadChildren: './recipes/recipes.module#RecipesModule',
+  // modern aprouch
+  loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)
+}
 ];
 
 @NgModule({
--- app-curso2/src/app/app.module.ts
+++ app-curso2/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { AuthComponent } from './auth/auth.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,7 +21,6 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule,
