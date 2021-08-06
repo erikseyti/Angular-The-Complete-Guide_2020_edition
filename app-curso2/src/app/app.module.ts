21ceb84c7b5bf22f5f158d8a29d7be44e8f2f60b
Erik Seyti <erikseyti@gmail.com>
2021-08-06 17:58:42 -0300

    On AppCurso2: Add NgRx Store-devTools

--- app-curso2/src/app/app.module.ts
+++ app-curso2/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { AuthEffects } from './auth/store/auth.effects';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
@@ -6,6 +7,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,6 +25,7 @@ import * as fromApp from './store/app.reducer';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule,
     CoreModule
   ],
