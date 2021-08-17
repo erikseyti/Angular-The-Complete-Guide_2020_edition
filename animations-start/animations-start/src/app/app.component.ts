4f0f83592051e5da0662c009d2f19b489571c3a7
Erik Seyti <erikseyti@gmail.com>
2021-08-17 14:28:08 -0300

    Using angular animation switch between two 2 states an object on the view

--- animations-start/animations-start/src/app/app.component.ts
+++ animations-start/animations-start/src/app/app.component.ts
@@ -1,13 +1,42 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          // backgroundColor: "blue",
+          'background-color': 'blue',
+          transform: 'translateX(100px)',
+        })
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.slice(this.list.indexOf(item), 1);
+  }
 }
